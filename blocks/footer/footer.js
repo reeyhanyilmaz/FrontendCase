d6f329d3538e20b92d4ac286a32499491991b35f
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2024-01-14 14:06:01 +0300

    add footer and style

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,21 +1,20 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+import { getMetadata } from "../../scripts/aem.js";
+import { loadFragment } from "../fragment/fragment.js";
 
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
 export default async function decorate(block) {
-  const footerMeta = getMetadata('footer');
-  block.textContent = '';
+  const footerMeta = getMetadata("footer");
+  block.textContent = "";
 
   // load footer fragment
-  const footerPath = footerMeta.footer || '/footer';
+  const footerPath = footerMeta.footer || "/footer";
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
-  const footer = document.createElement('div');
+  const footer = document.createElement("div");
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+  const sectionDiv = document.querySelector("footer .section");
+  sectionDiv.parentElement.classList.add("footer-section-parent");
 }
